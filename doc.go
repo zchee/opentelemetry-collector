ca0c017a9587bf267c31f16bd75ddaa390cb2667
Koichi Shiraishi <zchee.io@gmail.com>
2021-06-16 21:56:27 +0900

    doc: add package top-level synopsis
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ doc.go
@@ -0,0 +1,5 @@
+// SPDX-FileCopyrightText: 2021 The opentelemetry-collector Authors
+// SPDX-License-Identifier: BSD-3-Clause
+
+// Package opentelemetrycollectors collection of opentelemetry-collectors build by open-telemetry/opentelemetry-collector-builder.
+package opentelemetrycollectors
